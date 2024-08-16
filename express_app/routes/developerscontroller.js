57f88fcbca0f30092b28a10ec0b88cf669e840e0
Sudharsan Ganesan <sudharsang45@protonmail.com>
2024-08-16 15:22:33 -0400

    updated CRUD methods with mongodb data

--- express_app/routes/developerscontroller.js
+++ express_app/routes/developerscontroller.js
@@ -1,63 +1,105 @@
 var express = require('express');
 var router = express.Router();
-var client = require('../mongoConfig/developerConnection');
-
+const client = require('../mongoConfig/developerConnection');
+const { ObjectId } = require('mongodb');
 
 // Get all developers
 
 router.get('/all', async function(req, res, next) {
-    await client.connect();
-    const collection = client.db('developers').collection('developer');
-    const developers = await collection.find().toArray();
-    client.close();
-    res.send(developers);
+    try {
+        await client.connect();
+        const collection = client.db('developers').collection('developer');
+        const developers = await collection.find().toArray();
+        res.send(developers);
+    }
+    catch(err){
+        res.status(500).send("Error processing request");
+    }
 });
 
 // Get developer by ID
-router.get('/:id', function(req, res, next) {
-    const developer = developers.find( dev => dev.id === parseInt(req.params.id));
-    res.send(developer);
+router.get('/:_id', async function(req, res, next) {
+    try {
+        await client.connect();
+        const collection = client.db('developers').collection('developer');
+        const developer = await collection.findOne({ _id: new ObjectId(req.params._id) });
+        if (!developer) {
+            res.status(404).send('Developer not found');
+        }
+        else {
+            res.send(developer);
+        }
+    }
+    catch(err){
+        res.status(500).send("Error processing request");
+    }
 });
 
 // create developer
-router.post('/addDeveloper', function(req, res, next) {
-    const newDeveloper = {
-        id: developers.length + 1,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        favouriteLanguage: req.body.favouriteLanguage,
-        yearStarted: req.body.yearStarted
-    };
-    developers.push(newDeveloper);
-    res.status(201).send(newDeveloper);
+router.post('/addDeveloper', async function(req, res, next) {
+    try {
+        await client.connect();
+        const collection = client.db('developers').collection('developer');
+        if(!req.body.firstName || !req.body.lastName || !req.body.favouriteLanguage || !req.body.yearStarted) {
+            res.status(400).send('All fields are required');
+        }
+        else {
+            const newDeveloper = {
+                firstName: req.body.firstName,
+                lastName: req.body.lastName,
+                favouriteLanguage: req.body.favouriteLanguage,
+                yearStarted: req.body.yearStarted
+            };
+            await collection.insertOne(newDeveloper);
+            res.status(201).send(newDeveloper);
+        }
+    }
+    catch(err){
+        res.status(500).send("Error processing request");
+    }    
 });
 
 //update a developer
-router.put('/update', function(req, res, next) {
-    const developer = developers.find(dev => dev.id === parseInt(req.body.id));
-    if(!developer){
-        res.status(401).send('developer not found');
+router.put('/update', async function(req, res, next) {
+    try {
+        await client.connect();
+        const collection = client.db('developers').collection('developer');
+        const developer = await collection.findOne({ _id: new ObjectId(String(req.body._id))});
+        if(!developer){
+            res.status(404).send('developer not found');
+        }
+        else {
+            developer.firstName = req.body.firstName,
+            developer.lastName = req.body.lastName,
+            developer.favouriteLanguage = req.body.favouriteLanguage,
+            developer.yearStarted = req.body.yearStarted
+            collection.updateOne({ _id: developer._id}, { $set: developer});
+            res.send(developer)
+        } 
     }
-    else {
-        developer.firstName = req.body.firstName,
-        developer.lastName = req.body.lastName,
-        developer.favouriteLanguage = req.body.favouriteLanguage,
-        developer.yearStarted = req.body.yearStarted
-        res.send(developer)
-    }
+    catch(err){
+        res.status(500).send("Error processing request");
+    } 
 });
 
 // delete a developer
 
-router.delete('/delete', function(req, res, next){
-    const developer = developers.find(dev => dev.id === parseInt(req.body.id));
-    if(!developer){
-        res.status(404).send('developer not found');
+router.delete('/delete', async function(req, res, next){
+    try {
+        await client.connect();
+        const collection = client.db('developers').collection('developer');
+        const developer = await collection.findOne({ _id: new ObjectId(String(req.body._id))});
+        if(!developer){
+            res.status(404).send('developer not found');
+        }
+        else {
+            collection.deleteOne({ _id: developer._id });
+            res.status(201).send('developer deleted');
+        }
+
     }
-    else {
-        const index = developers.indexOf(developer);
-        developers.splice(index, 1);
-        res.send('developer deleted');
+    catch(err) {
+        res.status(500).send("Error processing request");
     }
 });
 
